a1bb27f208cfb3c2d9cb32f6ee6dd7d79185ec34
Sujal Pamrar <parmarsujal098@gmail.com>
2025-05-19 11:39:27 +0530

    completing

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,25 +1,15 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-// Configure Storage
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req,file,cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "profile_images", // Optional: name of folder in Cloudinary
+    allowed_formats: ["jpg", "jpeg", "png"],
+  },
 });
 
-// File filter
-const fileFilter = (req,file,cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-    }else{
-        cb(new Error('Only .jpeg, .jpg and .png formats are allowed'), false);
-    }
-};
+const upload = multer({ storage });
 
-const upload = multer({storage, fileFilter});
-
-module.exports =  upload;
+module.exports = upload;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -16,12 +16,12 @@ router.post("/login", loginUser);
 router.get("/profile", protect, getUserProfile),
 router.patch("/profile", protect, updateUserProfile);
 
-router.post("/upload-image", upload.single("image"), (req,res) => {
-  if (!req.file) {
-    return res.status(400).json({message: "No file uploaded"});
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file || !req.file.path) {
+    return res.status(400).json({ error: "Image upload failed" });
   }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-  res.status(200).json({imageUrl});
-})
+
+  res.status(200).json({ imageUrl: req.file.path }); // Cloudinary gives full URL
+});
 
 module.exports = router;
